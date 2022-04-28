8cc55a963e96054da92325b61a3c7b41a514ed6d
Nicholas Dougherty <nicholas.j.dougherty@gmail.com>
2022-04-27 21:34:34 -0500

    More information

--- prepare.py
+++ prepare.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from itertools import Counter 
 
 def prepare_data(df):
     # Still considering playing with this as date-time.
@@ -40,3 +41,43 @@ def create_time_df(df):
     df2 = df2.set_index('year').sort_index()
     
     return df2
+
+ef remove_columns(df, cols_to_remove):
+    '''
+    This function takes in a pandas dataframe and a list of columns to remove. It drops those columns from the original df and returns the df.
+    '''
+    df = df.drop(columns=cols_to_remove)
+    return df
+                 
+                 
+def handle_missing_values(df, prop_required_column=0.5 , prop_required_row=0.5):
+    '''
+    This function takes in a pandas dataframe, default proportion of required columns (set to 50%) and proprtion of required rows (set to 75%).
+    It drops any rows or columns that contain null values more than the threshold specified from the original dataframe and returns that dataframe.
+    
+    Prior to returning that data, it will print statistics and list counts/names of removed columns/row counts 
+    '''
+    original_cols = df.columns.to_list()
+    original_rows = df.shape[0]
+    threshold = int(round(prop_required_column * len(df.index), 0))
+    df = df.dropna(axis=1, thresh=threshold)
+    threshold = int(round(prop_required_row * len(df.columns), 0))
+    df = df.dropna(axis=0, thresh=threshold)
+    remaining_cols = df.columns.to_list()
+    remaining_rows = df.shape[0]
+    dropped_col_count = len(original_cols) - len(remaining_cols)
+    dropped_cols = list((Counter(original_cols) - Counter(remaining_cols)).elements())
+    print(f'The following {dropped_col_count} columns were dropped because they were missing more than {prop_required_column * 100}% of data: \n{dropped_cols}\n')
+    dropped_rows = original_rows - remaining_rows
+    print(f'{dropped_rows} rows were dropped because they were missing more than {prop_required_row * 100}% of data')
+    return df
+
+# combined in one function
+def data_prep(df, cols_to_remove=[], prop_required_column=0.5, prop_required_row=0.5):
+    '''
+    This function calls the remove_columns and handle_missing_values to drop columns that need to be removed. It also drops rows and columns that have more 
+    missing values than the specified threshold.
+    '''
+    df = remove_columns(df, cols_to_remove)
+    df = handle_missing_values(df, prop_required_column, prop_required_row)
+    return df
--- wrangle.py
+++ wrangle.py
@@ -1,15 +1,22 @@
+# standards and customs
+import pandas as pd
 import numpy as np
-import pandas as pd 
+import describe as tatl
+import wrangle as get
+import model as mod
+from collections import Counter
+# plotting and stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.stats as stats
+# feature selection, splitting, scaling
+from sklearn.feature_selection import RFE
 from sklearn.model_selection import train_test_split
-# import scaling methods
-from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler, QuantileTransformer
-# import modeling methods
-from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import MinMaxScaler
+# import evaluation metrics and modeling methods
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score, precision_score, recall_score, f1_score, accuracy_score
 from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import explained_variance_score
 
 def wrangle_df2(target):
     ''' 
@@ -139,6 +146,270 @@ def split_time_with_val(df):
     #print(f'Train: {train_time.shape}, Validate {validate_time.shape}, and Test {test_time.shape} are ready.\
     #\n Proceed with EDA.')
     return train_time, validate_time, test_time
+
+def stats_visualized(train):
+        # set alpha
+    α = .05
+    # conduct pearsonr test on independent and dependent variables.
+    r, p = stats.pearsonr(train.ctfp, train.both_sexes)
+    
+    # plotting the variables used in the aforementioned test
+    train.plot.scatter('ctfp', 'both_sexes')
+    # Setting the title based on data dictionary
+    plt.title('Total Factor Productivity (TFP) at Current PPP: Correlation to Suicide Rate')
+    # displaying r at X and y positions, to the thousandths-place.
+    plt.text(1, 40, f'r = {r:.3f}')
+    # Boolean test on whether p is less than alpha. if true, the correlation is significant.
+    print(f' P is less than alpha: {p < α}')
+    # show this plot.
+    plt.show()
+    # make space for the next 
+    print('')
+    
+    # same code, no need for code comments. Just replace the x value and name accordingly, as well as set grid.
+    r, p = stats.pearsonr(train.cwtfp, train.both_sexes)
+    
+    train.plot.scatter('cwtfp', 'both_sexes')
+    plt.title('Welfare-relevant TFP levels at Current PPP: Correlation to Suicide Rate')
+    plt.text(.9, 40, f'r = {r:.3f}')
+    print(f' P is less than alpha: {p < α}')
+    plt.show()
+    print('')
+    
+    r, p = stats.pearsonr(train.rwtfpna, train.both_sexes)
+    
+    train.plot.scatter('rwtfpna', 'both_sexes')
+    plt.title('Welfare-relevant TFP at constant national prices: Correlation to Suicide Rate')
+    plt.text(1.0, 40, f'r = {r:.3f}')
+    print(f' P is less than alpha: {p < α}')
+    plt.show()
+    print('')
+    
+    r, p = stats.pearsonr(train.labsh, train.both_sexes)
+    
+    train.plot.scatter('labsh', 'both_sexes')
+    plt.title('Share of labour compensation in GDP at current national prices')
+    plt.text(.63, 40, f'r = {r:.3f}')
+    print(f' P is less than alpha: {p < α}')
+    plt.show()
+    
+def test_lithuania(train):
+    alpha = 0.5
+    print('Conducting one-sample t-test on Lithuania')
+    lithuania = train[train.country == 'Lithuania'].both_sexes
+    overall_mean = train.both_sexes.mean()
+    
+    print(f'The mean suicide rate of all countries is: {overall_mean}')
+    
+    t, p = stats.ttest_1samp(lithuania, overall_mean)
+    
+    print(f't ~ {t:.03}, p ~ {p/2:.03}, alpha = {alpha} \n')
+    
+    if p/2 > alpha:
+        print("We fail to reject the Null Hypothesis")
+    elif t > 0:
+        print("We fail to reject the Null Hypothesis")
+    else:
+        print("We reject the Null Hypothesis")
+        
+    print('''
+    So although the suicide rate in Lithuania is decreasing, \n it is not doing so rapidly enough \n or substantially enough to vary significantly from the overall rate.''') 
+    
+def scale_data(X_train, X_validate, X_test, return_scaler=False):
+    '''
+    Scales the 3 data splits.
+    takes in the train, validate, and test data splits and returns their scaled counterparts.
+    If return_scaler is true, the scaler object will be returned as well.
+    Target is not scaled.
+    columns_to_scale was originally used to check whether los_angeles and orange would cause trouble
+    '''
+    columns_to_scale = X_train.columns
+    
+    X_train_scaled = X_train.copy()
+    X_validate_scaled = X_validate.copy()
+    X_test_scaled = X_test.copy()
+    
+    scaler = MinMaxScaler()
+    scaler.fit(X_train_scaled[columns_to_scale])
+    
+    X_train_scaled[columns_to_scale] = scaler.transform(X_train[columns_to_scale])
+    X_validate_scaled[columns_to_scale] = scaler.transform(X_validate[columns_to_scale])
+    X_test_scaled[columns_to_scale] = scaler.transform(X_test[columns_to_scale])
+    
+    if return_scaler:
+        return scaler, X_train_scaled, X_validate_scaled, X_test_scaled
+    else:
+        return X_train_scaled, X_validate_scaled, X_test_scaled
+    
+def select_rfe(X_train_scaled, y_train, k, return_rankings=False, model=LinearRegression()):
+    # Use the passed model, LinearRegression by default
+    rfe = RFE(model, n_features_to_select=k)
+     # fit the data using RFE
+    rfe.fit(X_train_scaled, y_train)
+    # transforming data using RFE
+    X_rfe = rfe.fit_transform(X_train_scaled, y_train)
+    # get mask of columns selected as list
+    feature_mask = X_train_scaled.columns[rfe.support_].tolist()
+    if return_rankings:
+        rankings = pd.Series(dict(zip(X_train_scaled.columns, rfe.ranking_)))
+        return feature_mask, rankings, X_rfe
+    else:
+        return feature_mask, X_rfe
+    
+def all_models(): 
+    train, validate, test, X_train, y_train, X_validate, y_validate, X_test, y_test = get.wrangle_df2('both_sexes')
+    X_train_scaled, X_validate_scaled, X_test_scaled = get.scale_data(X_train, X_validate, X_test)
+    feature_mask = ['ctfp', 'cwtfp', 'rwtfpna', 'labsh', 'pl_con', 'pl_gdpo', 'pl_c', 'pl_g']
+    baseline = pd.DataFrame({
+            'median' : [y_train.median()] * y_train.size,
+            'mean' : [y_train.mean()] * y_train.size
+        })
+
+    median_rmse = mod._RMSE(y_train, baseline["median"])
+    mean_rmse = mod._RMSE(y_train, baseline["mean"])
+    baseline_val = pd.Series([y_train.mean()] * y_validate.size)
+
+    results = {
+        'baseline' : {
+            'RMSE' : mean_squared_error(y_train, baseline['mean'], squared = False),
+            'R^2' : r2_score(y_train, baseline['mean']),
+            'RMSE_val' : mean_squared_error(y_validate, baseline_val, squared = False),
+            'R^2_val' : r2_score(y_validate, baseline_val)
+        }
+    }
+    results
+    
+    model = LinearRegression()
+    model.fit(X_train[feature_mask], y_train)
+
+    r2_score(y_train, model.predict(X_train[feature_mask]))
+    mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False)
+    results['linear_regression'] = {
+    'RMSE' : mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False),
+    'R^2' : r2_score(y_train, model.predict(X_train[feature_mask])),
+    'RMSE_val' : mean_squared_error(y_validate, model.predict(X_validate[feature_mask]), squared = False),
+    'R^2_val' : r2_score(y_validate, model.predict(X_validate[feature_mask]))
+    }
+    
+    # OLS
+    model = LinearRegression(normalize=True)
+    model.fit(X_train[feature_mask], y_train)
+
+    r2_score(y_train, model.predict(X_train[feature_mask]))
+    
+    mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False)
+    
+    results['linear_regression_OLS'] = {
+    'RMSE' : mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False),
+    'R^2' : r2_score(y_train, model.predict(X_train[feature_mask])),
+    'RMSE_val' : mean_squared_error(y_validate, model.predict(X_validate[feature_mask]), squared = False),
+    'R^2_val' : r2_score(y_validate, model.predict(X_validate[feature_mask]))
+    }
+    
+    # Polynomial Regression
+    poly = PolynomialFeatures(degree = 2, include_bias = False, interaction_only = False)
+    poly.fit(X_train[feature_mask])
+
+    X_train_poly = pd.DataFrame(
+        poly.transform(X_train[feature_mask]),
+        columns = poly.get_feature_names(X_train[feature_mask].columns),
+        index = X_train[feature_mask].index
+    )
+    
+    model = LinearRegression()
+    model.fit(X_train_poly, y_train)
+    
+    r2_score(y_train, model.predict(X_train_poly))
+    
+    mean_squared_error(y_train, model.predict(X_train_poly), squared = False)
+    
+    # Before we make predictions on validate we need to prepare the X_validate set with the polynomial features.
+    poly = PolynomialFeatures(degree = 2, include_bias = False, interaction_only = False)
+    poly.fit(X_validate[feature_mask])
+    
+    X_validate_poly = pd.DataFrame(
+        poly.transform(X_validate[feature_mask]),
+        columns = poly.get_feature_names(X_validate[feature_mask].columns),
+        index = X_validate[feature_mask].index
+    )
+    
+    results['polynomial_regression'] = {
+    'RMSE' : mean_squared_error(y_train, model.predict(X_train_poly), squared = False),
+    'R^2' : r2_score(y_train, model.predict(X_train_poly)),
+    'RMSE_val' : mean_squared_error(y_validate, model.predict(X_validate_poly), squared = False),
+    'R^2_val' : r2_score(y_validate, model.predict(X_validate_poly))
+    }
+    
+    # Polynomial Regression with Interactions Only
+    poly = PolynomialFeatures(degree = 2, include_bias = False, interaction_only = True)
+    poly.fit(X_train[feature_mask])
+    
+    X_train_poly = pd.DataFrame(
+        poly.transform(X_train[feature_mask]),
+        columns = poly.get_feature_names(X_train[feature_mask].columns),
+        index = X_train[feature_mask].index
+    )
+    
+    model = LinearRegression()
+    model.fit(X_train_poly, y_train)
+    
+    r2_score(y_train, model.predict(X_train_poly))
+    
+    mean_squared_error(y_train, model.predict(X_train_poly), squared = False)
+    
+    # We have to prepare the validate set again
+
+    poly = PolynomialFeatures(degree = 2, include_bias = False, interaction_only = True)
+    poly.fit(X_validate[feature_mask])
+    
+    X_validate_poly = pd.DataFrame(
+        poly.transform(X_validate[feature_mask]),
+        columns = poly.get_feature_names(X_validate[feature_mask].columns),
+        index = X_validate[feature_mask].index
+    )
+    
+    results['polynomial_regression_interactions_only'] = {
+    'RMSE' : mean_squared_error(y_train, model.predict(X_train_poly), squared = False),
+    'R^2' : r2_score(y_train, model.predict(X_train_poly)),
+    'RMSE_val' : mean_squared_error(y_validate, model.predict(X_validate_poly), squared = False),
+    'R^2_val' : r2_score(y_validate, model.predict(X_validate_poly))
+    }
+    
+    # Tweedie with Power of 1: Poisson distribution
+    # No collinearities from RFE is assumed, so alpha 0
+
+    model = TweedieRegressor(power = 1, alpha = 0)
+    model.fit(X_train[feature_mask], y_train)
+
+    r2_score(y_train, model.predict(X_train[feature_mask]))
+    
+    mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False)
+    
+    results['tweedie_regressor_poisson'] = {
+    'RMSE' : mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False),
+    'R^2' : r2_score(y_train, model.predict(X_train[feature_mask])),
+    'RMSE_val' : mean_squared_error(y_validate, model.predict(X_validate[feature_mask]), squared = False),
+    'R^2_val' : r2_score(y_validate, model.predict(X_validate[feature_mask]))
+    }
+    
+    # Tweedie with Power of 1.5: Compound Poisson Gamma distribution
+    # No collinearities from RFE is assumed, so alpha 0
+
+    model = TweedieRegressor(power = 1.5, alpha = 0)
+    model.fit(X_train[feature_mask], y_train)
+
+    r2_score(y_train, model.predict(X_train[feature_mask]))
+    
+    mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False)
+    
+    results['tweedie_regressor_compound_poisson_gamma'] = {
+    'RMSE' : mean_squared_error(y_train, model.predict(X_train[feature_mask]), squared = False),
+    'R^2' : r2_score(y_train, model.predict(X_train[feature_mask])),
+    'RMSE_val' : mean_squared_error(y_validate, model.predict(X_validate[feature_mask]), squared = False),
+    'R^2_val' : r2_score(y_validate, model.predict(X_validate[feature_mask]))
+    }
+    
+    return results
 
 # Keeping for my own records, but random selection of each is a bad idea when there's value gained from the time element. 
 # def split_suicide(df):
